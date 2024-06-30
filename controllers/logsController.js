8a2d595afb0934a583ba19ec80608b0610b4cdd9
alexandra-perez <alexandraperezny@gmail.com>
2024-06-30 13:04:15 -0400

    Add GET /:id to get log by specific ID

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,16 +2,32 @@ const express = require('express');
 const router = express.Router();
 const logArray = require('../models/log');
 
+// Index
 router.get('/', (req, res) => {
   res.status(200).send(logArray);
-  next()
+  next();
 });
 
-router.post("/", (req, res) => {
-  const currentLog = { id: logArray.length + 1, ...req.body };
-  logArray.push(currentLog);
-  res.send(logArray[logArray.length - 1]);
-})
+// Show
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const log = logArray.find((log) => log.id === parseInt(id));
+  if (log !== undefined) {
+    res.status(200).send(log);
+  } else {
+    res.status(404).send(`No log with ID ${id} found.`);
+  }
+});
 
+// Create
+router.post('/', (req, res) => {
+  const currentLog = req.body;
+  logArray.push(currentLog);
+  res.status(200).send(logArray[logArray.length - 1]);
+});
+
+// Destroy
+
+// Update
 
 module.exports = router;
